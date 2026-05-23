29ae3208f5ec5e3bc3ac64d26fa9c46a9401594c
z85280129-tech <z85280129@gmail.com>
2026-05-23 22:49:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/8bq/'
+    base: '/8bq/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
